26b7baf8ee52586c269bcfe052cb83eacaf723a8
Mohammed Irfan <irfanmk1999@gmail.com>
2024-11-24 11:30:45 +0530

    Updated routing syntax to use React Router v6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes and Route
+import "./styles.css";
+import SurahList from "./SurahList";
+import About from "./About";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<SurahList />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Router>,
+  document.getElementById("root")
+);
