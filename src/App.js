cebfbe99bf41ca713193a3bd45b90131c300b593
Mohammed Irfan <irfanmk1999@gmail.com>
2024-11-28 00:12:27 +0530

    Deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SurahList from "./SurahList"; // Home page or SurahList component
 import About from "./About"; // About page component
 import Header from "./Header"; // Header component
-=======
-import React, { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import SurahList from './SurahList'; // Your home page component
-import About from './About'; // About page component
-import Header from './Header'; // Header component for navigation
-import Footer from './Footer'; // Footer component
->>>>>>> Try
+import Footer from "./Footer"; // Footer component
 
 const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false); // State for managing the menu
 
   return (
-<<<<<<< HEAD
-    <Router>
-      {/* Header is common across all pages */}
-      <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-
-      {/* Routes for different pages */}
-      <Routes>
-        {/* Route for SurahList/Home page */}
-        <Route path="/" element={<SurahList />} />
-
-        {/* Route for the About page */}
-        <Route path="/about" element={<About />} />
-=======
     <>
       <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <Routes>
@@ -37,11 +16,7 @@ const App = () => {
           path="/"
           element={<SurahList isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />}
         />
-        <Route
-          path="/about"
-          element={<About />}
-        />
->>>>>>> Try
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </>
