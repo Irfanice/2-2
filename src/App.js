affcb52c7ce82f13f7541216940171dc45c38cdc
Mohammed Irfan <irfanmk1999@gmail.com>
2024-11-28 12:29:19 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Utility function to toggle the visibility of an element
+export function toggleVisibility(element) {
+  if (element.classList.contains('hidden')) {
+      element.classList.remove('hidden');
+  } else {
+      element.classList.add('hidden');
+  }
 }
-
-export default App;
