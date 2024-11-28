c6fb6dcc9658bd8f9f5014a279f0ed24d96c43c9
Mohammed Irfan <irfanmk1999@gmail.com>
2024-11-28 07:19:47 +0530

    Deploy latest build to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';  // Specific styles for the index (global styles or overrides)
+import './styles.css'; // Import your global styles
+import SurahList from './SurahList'; // Main page component
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <SurahList />
+  </React.StrictMode>,
+  document.getElementById('root') // This is where your app will be displayed in the HTML
+);
