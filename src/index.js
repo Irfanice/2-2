97bb7294e29cc14c620f6bce72997056f32705ec
Mohammed Irfan <irfanmk1999@gmail.com>
2024-11-28 12:57:52 +0530

    message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import { toggleVisibility } from './App.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const toggleButton = document.getElementById('toggleButton');
-const message = document.getElementById('message');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-toggleButton.addEventListener('click', () => {
-    toggleVisibility(message);
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
